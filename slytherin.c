287edca8865ca07584a6c340aa8b1f5e5d858f59
Yehya <yehyalbakri@gmail.com>
2021-03-17 11:24:21 -0400

    display moving snake

--- slytherin.c
+++ slytherin.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
 // #include <mem.h>
 
 void printToCoordinates(int y, int x, char text[])
@@ -17,7 +18,7 @@ struct coordinate
 };
 
 struct coordinate coordinate;
-struct coordinate head;
+// struct coordinate head;
 
 struct Node
 {
@@ -28,7 +29,7 @@ struct Node
     struct Node *head;
     // struct Node *tail;
 };
-struct Node *snake; /* Global variable - pointer to snake node */
+struct Node *head; /* Global variable - pointer to head node */
 
 struct Node *getNewNode(int x, int y)
 {
@@ -42,55 +43,90 @@ struct Node *getNewNode(int x, int y)
     return newNode;
 }
 
-void insertAtHead(int x, int y, char *xs, char *ys)
+void insertAtHead(int x, int y)
 {
     struct Node *newNode = getNewNode(x, y);
-    if ((*newNode).head == NULL)
+    if (head == NULL)
     {
         // struct Node *newNode = getNewNode(5, 5);
-        (*newNode).head = newNode;
+        head = newNode;
         return;
     }
-    (*newNode).head->prev = newNode;
-    (*newNode).next = (*newNode).head;
-    (*newNode).head = newNode;
+    head->prev = newNode;
+    (*newNode).next = head;
+    head = newNode;
     (*newNode).x = x;
     (*newNode).y = y;
 
-    // char *xs = malloc(30);
-    // char *ys = malloc(30);
-    struct Node temp = *newNode;
-    xs[0] = 10;
-    ys[0] = 25;
-    for (int i = 0; i < 30; i++)
-    {
-        // xs[i] = temp.x;
-        // ys[i] = temp.y;
-        if (temp.next == NULL)
-        {
-            return;
-        }
-        temp = *temp.next;
-    }
+    // struct Node temp = *newNode;
+    // for (int i = 0; i < 30; i++)
+    // {
+    //     xs[i] = temp.x;
+    //     ys[i] = temp.y;
+    //     if (temp.next == NULL)
+    //     {
+    //         return;
+    //     }
+    //     temp = *temp.next;
+    //     // temp.x += 1;
+    // }
 }
 
-int length;
+void print()
+{
+    struct Node *temp = head;
+    // printf("Forward: ");
+    while (temp != NULL)
+    {
+        printToCoordinates(temp->x, temp->y, "*");
+        // printf("%d ", temp->x);
+        temp = temp->next;
+        usleep(200000);
+    }
+    // printf("\n");
+}
 
-/*  */
 int main()
 {
+    int length;
     system("clear"); /* Clears the terminal */
     length = 5;
     // head.x = 25;
     // head.y = 10;
-    char xs[30];
-    char ys[30];
-    printf("%d\n", xs[0]);
-    insertAtHead(25, 10, xs, ys);
-    printf("%d\n ", xs[0]);
+    // int xs[30];
+    // int ys[30];
+    // printf("x: %d, y: %d\n\n", xs[0], ys[0]);
+    // struct Node *newNode = getNewNode(10, 25);
+    insertAtHead(11, 25);
+    insertAtHead(12, 25);
+    insertAtHead(13, 25);
+    insertAtHead(14, 25);
+
+    print();
+
+    // struct Node temp = *newNode;
+    // for (int i = 0; i < 30; i++)
+    // {
+    //     xs[i] = temp.x;
+    //     ys[i] = temp.y;
+    //     if (temp.next == NULL)
+    //     {
+    //         return;
+    //     }
+    //     temp = *temp.next;
+    //     // temp.x += 1;
+    // }
+    // insertAtHead(11, 25, xs, ys);
+    // insertAtHead(12, 25, xs, ys);
     // xs[0] = 10;
     // ys[0] = 25;
-    printToCoordinates(xs[0], ys[0], "*");
+    // printf("x: %d, y: %d\n", xs[0], ys[0]);
+    // xs[0] = 10;
+    // ys[0] = 25;
+    // printToCoordinates(xs[0], ys[0], "*");
+    // printToCoordinates(xs[1], ys[1], "*");
+    // printToCoordinates(xs[2], ys[2], "*");
+    // printToCoordinates(xs[3], ys[3], "*");
     load(); /* Prints game board */
 }
 
