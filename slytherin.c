3f8392b1eb6786afd89c157a1a264490b21f2745
Yehya <yehyalbakri@gmail.com>
2021-03-16 22:24:13 -0400

    added DLL

--- slytherin.c
+++ slytherin.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <ctype.h>
+// #include <mem.h>
 
 void printToCoordinates(int y, int x, char text[])
 {
@@ -16,15 +17,81 @@ struct coordinate
 };
 
 struct coordinate coordinate;
+struct coordinate head;
+
+struct Node
+{
+    int x;
+    int y;
+    struct Node *next;
+    struct Node *prev;
+    struct Node *head;
+    // struct Node *tail;
+};
+struct Node *snake; /* Global variable - pointer to snake node */
+
+struct Node *getNewNode(int x, int y)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    (*newNode).x = x;
+    (*newNode).y = y;
+    (*newNode).prev = NULL;
+    (*newNode).next = NULL;
+    (*newNode).head = NULL;
+    // (*newNode).tail = NULL;
+    return newNode;
+}
+
+void insertAtHead(int x, int y, char *xs, char *ys)
+{
+    struct Node *newNode = getNewNode(x, y);
+    if ((*newNode).head == NULL)
+    {
+        // struct Node *newNode = getNewNode(5, 5);
+        (*newNode).head = newNode;
+        return;
+    }
+    (*newNode).head->prev = newNode;
+    (*newNode).next = (*newNode).head;
+    (*newNode).head = newNode;
+    (*newNode).x = x;
+    (*newNode).y = y;
+
+    // char *xs = malloc(30);
+    // char *ys = malloc(30);
+    struct Node temp = *newNode;
+    xs[0] = 10;
+    ys[0] = 25;
+    for (int i = 0; i < 30; i++)
+    {
+        // xs[i] = temp.x;
+        // ys[i] = temp.y;
+        if (temp.next == NULL)
+        {
+            return;
+        }
+        temp = *temp.next;
+    }
+}
 
 int length;
 
+/*  */
 int main()
 {
-    system("clear");
-
-    load();
-    // printToCoordinates(5, 15, "hello");
+    system("clear"); /* Clears the terminal */
+    length = 5;
+    // head.x = 25;
+    // head.y = 10;
+    char xs[30];
+    char ys[30];
+    printf("%d\n", xs[0]);
+    insertAtHead(25, 10, xs, ys);
+    printf("%d\n ", xs[0]);
+    // xs[0] = 10;
+    // ys[0] = 25;
+    printToCoordinates(xs[0], ys[0], "*");
+    load(); /* Prints game board */
 }
 
 void load()
